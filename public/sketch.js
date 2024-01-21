1bc710d3a5289661bdb659c8fcc5b31f1e9cb5ad
Priyabrata Das <priyobapi@gmail.com>
2024-01-21 16:24:21 +0530

    Added some feature like Pen-Color Changer, Eraser, Strength of Pen & Eraser

--- public/sketch.js
+++ public/sketch.js
@@ -1,25 +1,30 @@
 let socket;
 let eraseme = false;
+let drawingStroke = 2;
+let eraserStroke = 10;
+let strokeColor = "black";
+let backgroundColor = "white";
+
 
 function setup() {
     createCanvas(displayWidth, displayHeight);
-    strokeWeight(3);
-    stroke('black');
+    background(backgroundColor);
+    strokeWeight(drawingStroke);
+    stroke(strokeColor);
 
     socket = io.connect('http://localhost:3000');
     socket.on('mouse',(data) => {
         if(data.era){
             erase(255,255);
+            strokeWeight(eraserStroke);
             line(data.x, data.y, data.px, data.py);
             noErase();
         }
         else{
+            strokeWeight(data.drawingStroke);
+            stroke(data.strokeColor);
             line(data.x, data.y, data.px, data.py);
         }
-
-        
-        
-
     });
       
 }
@@ -27,10 +32,13 @@ function setup() {
 function touchMoved() {
     if(eraseme){
         erase(255,255);
+        strokeWeight(eraserStroke);
         line(mouseX, mouseY, pmouseX, pmouseY);
         noErase();
     }
     else{
+        strokeWeight(drawingStroke);
+        stroke(strokeColor);
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
     
@@ -39,7 +47,9 @@ function touchMoved() {
         y : mouseY,
         px : pmouseX,
         py : pmouseY,
-        era: eraseme
+        era: eraseme,
+        drawingStroke: drawingStroke,
+        strokeColor: strokeColor
     }
     
     socket.emit('mouse',data);
@@ -47,10 +57,29 @@ function touchMoved() {
     
 }
 
+const colorButtons = document.querySelectorAll('.color-button');
+const penStrengthSlider = document.getElementById('pen-strength');
+const eraserStrengthSlider = document.getElementById('eraser-strength');
 
-const e = document.getElementById('eraser');
-e.addEventListener('click',(event)=>{
-    console.log('Clicked');
-    console.log(eraseme);
-    eraseme = !eraseme;
-})
+
+colorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Handle color button click, you can use the button's background color
+        strokeColor = String(button.style.backgroundColor);
+        console.log('Selected color:', button.style.backgroundColor);
+    });
+});
+
+penStrengthSlider.addEventListener('input', () => {
+// Handle pen strength slider change
+    drawingStroke = penStrengthSlider.value;
+    eraseme = false;
+    console.log('Pen Strength:', penStrengthSlider.value);
+});
+
+eraserStrengthSlider.addEventListener('input', () => {
+// Handle eraser strength slider change
+    eraseme = true;
+    eraserStroke = eraserStrengthSlider.value;
+    console.log('Eraser Strength:', eraserStrengthSlider.value);
+});
